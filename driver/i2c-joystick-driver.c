cca61238e1503f332c5ea7a6a00e71e43c4ac65e
Fodor Norbert <n.fodor@mito.hu>
2018-02-27 15:34:22 +0100

    controller config

--- driver/i2c-joystick-driver.c
+++ driver/i2c-joystick-driver.c
@@ -11,10 +11,6 @@
 
 typedef struct {
   uint16_t buttons; // button status
-  uint8_t axis0; // first axis
-  uint8_t axis1; // second axis
-  uint8_t powerDownRequest; // when true, request the pi to shutdown
-  uint8_t audioVolume;
 } I2CJoystickStatus;
 
 int readI2CJoystick(int file, I2CJoystickStatus *status) {
@@ -33,67 +29,21 @@ void updateUInputDevice(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystic
   TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 1, BTN_B);
   TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 2, BTN_X);
   TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 3, BTN_Y);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 4, BTN_TL);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 5, BTN_TR);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 6, BTN_START);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 7, BTN_SELECT);
-
-  // joystick axis 
-  if(newStatus->axis0 != status->axis0) {
-    // send event
-    int val = newStatus->axis0;
-    val = (val - 128) * 4;
-    sendInputEvent(UInputFIle, EV_ABS, ABS_X, val);
-  }
-
-  if(newStatus->axis1 != status->axis1) {
-    // send event
-    int val = newStatus->axis1;
-    val = (val - 128) * 4;
-    sendInputEvent(UInputFIle, EV_ABS, ABS_Y, val);
-  }
-}
-
-#include <alsa/asoundlib.h>
-// input volume : [0..100]
-// XXX maybe need some tweak!
-// input value should be a dB value, not a % of the dB range
-void SetAlsaMasterVolume(long volume)
-{
-    long min, max;
-    snd_mixer_t *handle;
-    snd_mixer_selem_id_t *sid;
-    const char *card = "default";
-    const char *selem_name = "PCM";
-
-    snd_mixer_open(&handle, 0);
-    snd_mixer_attach(handle, card);
-    snd_mixer_selem_register(handle, NULL, NULL);
-    snd_mixer_load(handle);
-
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
-
-    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-
-    float v = (float)volume * 0.01 * (float)(max - min) + min;
-    snd_mixer_selem_set_playback_volume_all(elem, v);
-    snd_mixer_close(handle);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 4, BTN_UP);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 5, BTN_DOWN);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 6, BTN_LEFT);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 7, BTN_RIGHT);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 8, BTN_START);
+  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 9, BTN_SELECT);
 }
 
 int main(int argc, char *argv[]) {
   // open I2C device  
-  int I2CFile = openI2C(1);
+  int I2CFile = openI2C(3);
 
   // current joystick status
   I2CJoystickStatus status;
   status.buttons = 0;
-  status.axis0 = 127;
-  status.axis1 = 127;
-  status.powerDownRequest = 0;
-  status.audioVolume = -1; // so it will be set at first request
 
   // create uinput device
   int UInputFIle = createUInputDevice();
@@ -108,16 +58,6 @@ int main(int argc, char *argv[]) {
     } else {
       // everything is ok
       updateUInputDevice(UInputFIle, &newStatus, &status);
-
-      if(newStatus.powerDownRequest) {
-	// halt!
-	system("halt");
-      }
-
-      if(newStatus.audioVolume != status.audioVolume) {
-	SetAlsaMasterVolume(newStatus.audioVolume);
-      }
-
       status = newStatus;
     }
 
